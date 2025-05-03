aebe95bcddf11b0d1af9090534d8914481ad101f
Learner062022 <ddylan246@yahoo.com>
2025-05-03 22:12:15 +0800

    feat(favourites): add Favourites page with model binding and navigation
    Implements the Favourites page with model binding for state management. Adds navigation support via a back button. Prepares ListView item selection handling for future enhancements.

---
+++ DylanDeSouzaQuoteAppPart1/Favourites.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DylanDeSouzaQuoteAppPart1
+{
+    public partial class Favourites : ContentPage
+    {
+        Model model;
+
+        public Favourites(Model model)
+        {
+            InitializeComponent();
+            this.model = model;
+            BindingContext = model;
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "Back") Navigation.PopAsync();
+        }
+
+        void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
