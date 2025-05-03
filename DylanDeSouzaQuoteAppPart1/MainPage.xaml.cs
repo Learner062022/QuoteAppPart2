724a33dcab66fdfb5e1c2f20208ae3defcfa5d8b
Learner062022 <ddylan246@yahoo.com>
2025-05-03 22:01:11 +0800

    feat(main-page): add navigation to Favourites page
    Integrates a Favourites page instance into MainPage and adds navigation logic to push the page when the corresponding button is clicked. Sets up groundwork for future functionality related to marking quotes as favourites.

--- DylanDeSouzaQuoteAppPart1/MainPage.xaml.cs
+++ DylanDeSouzaQuoteAppPart1/MainPage.xaml.cs
@@ -7,19 +7,21 @@ namespace DylanDeSouzaQuoteAppPart1
     public partial class MainPage : ContentPage
     {
         Model model = new();
+        Favourites favourites;
 
         public MainPage()
         {
             InitializeComponent();
             BindingContext = model;
+            favourites = new(model);
         }
         
-
         void Button_Clicked(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             if (button.Text == "Add Quote") model.AddQuote();
             else if (button.Text == "Get Random Quote") model.FetchRandomQuote();
+            else if (button.Text == "Favourites") Navigation.PushAsync(favourites);
         }
     }
 }
